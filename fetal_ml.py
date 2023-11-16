1404864720c06becbaa4723083aaa5621f83e130
”aebelden4” <aebelden@calpoly.edu>
2023-11-16 15:58:54 -0800

    updated imports pt 6

--- fetal_ml.py
+++ fetal_ml.py
@@ -6,9 +6,6 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-import sklearn
-from sklearn.ensemble import RandomForestClassifier
-
 import warnings
 warnings.filterwarnings('ignore')
 
